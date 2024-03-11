46e422d0c848923eb17b6f2425f256854910bd85
Joaquín <joaquinmendozasebastian@gmail.com>
2024-03-11 14:54:39 +0100

    fin de api colores

---
+++ db.js
@@ -0,0 +1,68 @@
+require("dotenv").config();
+const { MongoClient,ObjectId } = require("mongodb");
+
+function conectar(){
+    return MongoClient.connect(process.env.URL_DB);
+}
+
+function leerColores(){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            let coleccion = conexion.db("colores").collection("colores");
+
+            let colores =  await coleccion.find({}).toArray();
+
+            conexion.close();
+
+            ok(colores);
+
+        }catch(error){
+            ko({ error : "error en BBDD" });
+        }
+    });
+}
+
+function crearColor(color){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            let coleccion = conexion.db("colores").collection("colores");
+
+            let {insertedId} =  await coleccion.insertOne(color);
+
+            conexion.close();
+
+            ok({ id : insertedId });
+
+        }catch(error){
+            ko({ error : "error en BBDD" });
+        }
+    });
+}
+
+function borrarColor(id){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            let coleccion = conexion.db("colores").collection("colores");
+
+            let {deletedCount} =  await coleccion.deleteOne({ _id : new ObjectId(id) });
+
+            conexion.close();
+
+            ok(deletedCount);
+
+        }catch(error){
+            ko({ error : "error en BBDD" });
+        }
+    });
+}
+
+module.exports = {leerColores,crearColor,borrarColor};
+
+
+
--- index.js
+++ index.js
@@ -1,19 +1,79 @@
 require("dotenv").config();
+
 const express = require("express");
+const {json} = require("body-parser");
+const cors = require("cors");
+const {leerColores,crearColor,borrarColor} = require("./db");
 
 const servidor = express();
 
+servidor.use(cors());
 
-servidor.get("/colores", (peticion,respuesta) => {
-    respuesta.send("..get");
+servidor.use(json());
+
+servidor.use("/mentirillas", express.static("./pruebas"));
+
+
+servidor.get("/colores", async (peticion,respuesta) => {
+    try {
+        let colores = await leerColores();
+
+        colores = colores.map(({_id,r,g,b}) => { return { id : _id,r,g,b } });
+
+        respuesta.json(colores);
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json(error);
+    }  
 });
 
-servidor.post("/colores/nuevo", (peticion,respuesta) => {
-    respuesta.send("...post");
+servidor.post("/colores/nuevo",async (peticion,respuesta,siguiente) => {
+
+    let {r,g,b} = peticion.body;
+
+    let valido = true;
+
+    [r,g,b].forEach( n => valido = valido && n >= 0 && n <= 255);
+
+    if(valido){
+        try{
+
+            let resultado = await crearColor({r,g,b});
+
+            return respuesta.json(resultado);
+
+        }catch(error){
+            respuesta.status(500);
+
+            return respuesta.json(error);
+        }
+    }
+
+    siguiente({ error : "faltan parámetros" });
+  
 });
 
-servidor.delete("/colores/borrar/:id", (peticion,respuesta) => {
-    respuesta.send("..delete");
+servidor.delete("/colores/borrar/:id([a-f0-9]{24})", async (peticion,respuesta) => {
+    try{
+        let cantidad = await borrarColor(peticion.params.id);
+
+        respuesta.json({ resultado : cantidad > 0 ? "ok" : "ko" });
+
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error);
+    }
+});
+
+servidor.use((error,peticion,respuesta,siguiente) => {
+    respuesta.status(400);
+    respuesta.json({ error : "error en la petición" });
+});
+
+servidor.use((peticion,respuesta) => {
+    respuesta.status(404);
+    respuesta.json({ error : "recurso no encontrado" });
 });
 
 
